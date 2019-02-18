ac12dc7f27f712f8a15b3b99a92f408810d9b802
so kesseler <apk5@illinois.edu>
2019-02-17 23:56:57 -0600

    commit

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
@@ -1,4 +1,19 @@
 package edu.illinois.cs.cs125.spring2019.mp1.lib;
-
+/**
+Transform.
+ */
 public class Transform {
+    /**
+    Image.
+     */
+    private int[][] image;
+    /**
+    Shifts left.
+     @param array given array.
+     @param shift how much to shift.
+     @return Shifted array.
+     */
+    public int[][] shiftLeft(final int[][] array, final int shift) {
+        return array;
+    }
 }
