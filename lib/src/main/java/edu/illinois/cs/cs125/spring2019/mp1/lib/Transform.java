2275c4b579069643f4f9fcc486e2a9e8be62a2b9
so kesseler <apk5@illinois.edu>
2019-02-18 00:19:45 -0600

    commit3

--- lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
+++ lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp1/lib/Transform.java
@@ -27,4 +27,67 @@ public class Transform {
         }
         return newArray;
     }
+
+    /**
+     Shifts right.
+     @param array given array.
+     @param shift how much to shift.
+     @return Shifted array.
+     */
+    public int[][] shiftRight(final int[][] array, final int shift) {
+        int[][] newArray = new int[array.length][array[0].length];
+        int move = shift % array[0].length;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array[x].length; y++) {
+                if (array[x][y - move] < array[x].length) {
+                    newArray[x][y] = array[x][y - move + array[x].length];
+                } else {
+                    newArray[x][y] = array[x][y - move];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    /**
+     Shifts up.
+     @param array given array.
+     @param shift how much to shift.
+     @return Shifted array.
+     */
+    public int[][] shiftUp(final int[][] array, final int shift) {
+        int[][] newArray = new int[array.length][array[0].length];
+        int move = shift % array.length;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array[x].length; y++) {
+                if (array[x - move][y] < array.length) {
+                    newArray[x][y] = array[x - move + array.length][y];
+                } else {
+                    newArray[x][y] = array[x - move][y];
+                }
+            }
+        }
+        return newArray;
+    }
+
+    /**
+     Shifts down.
+     @param array given array.
+     @param shift how much to shift.
+     @return Shifted array.
+     */
+    public int[][] shiftDown(final int[][] array, final int shift) {
+        int[][] newArray = new int[array.length][array[0].length];
+        int move = shift % array.length;
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array[x].length; y++) {
+                if (array[x + move][y] > array.length) {
+                    newArray[x][y] = array[x + move - array.length][y];
+                } else {
+                    newArray[x][y] = array[x + move][y];
+                }
+            }
+        }
+        return newArray;
+    }
 }
